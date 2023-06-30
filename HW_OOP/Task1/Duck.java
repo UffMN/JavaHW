3c7e7efb7a765011c095b35a18394c6dcbc1b5ad
MN <mattheykun@gmail.com>
2023-07-01 04:21:19 +0500

    Add homework OOP Task1 Java

---
+++ HW_OOP/Task1/Duck.java
@@ -0,0 +1,10 @@
+public class Duck extends Animal{
+
+    public Duck(String name, String owner, String species, Integer age, String color, String vaccinate,
+            Integer legsCount) {
+        super(name, owner, species, age, color, vaccinate, legsCount);
+        
+    }
+
+    
+}
